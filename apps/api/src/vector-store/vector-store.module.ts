02445610997b90c13ac450e69a8ad2b4d3af4f56
Van Allen Diongzon <vanallendiongzon@Vans-MacBook-Pro.local>
2023-12-25 18:28:59 +0800

    [Adjustment][Van] Add ai config

--- apps/api/src/vector-store/vector-store.module.ts
+++ apps/api/src/vector-store/vector-store.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { VectorStoreController } from './vector-store.controller';
 import { VectorStoreService } from './vector-store.service';
 import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule } from '@nestjs/config';
+import { aiConfig } from '../common/configs/ai-config.config';
 
 @Module({
-  imports: [MulterModule.register()],
+  imports: [MulterModule.register(), ConfigModule.forFeature(aiConfig)],
   controllers: [VectorStoreController],
   providers: [VectorStoreService],
 })
